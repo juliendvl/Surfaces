4d5625e223f5656e02c8e890ec7a2ddd7c29fa14
Maguelonne <maguelonne.beaud-de-brive@inria.fr>
2017-09-06 17:51:01 +0200

    draw example of hermite surface

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "surfaces/Grid.h"
+#include "surfaces/HermiteSurface.h"
 #include "viewer/Viewer.h"
 
 
@@ -12,12 +13,16 @@ int main()
     viewer.addProgram("curve", "../Shaders/curve.vs", "../Shaders/curve.fs");
     viewer.addProgram("point", "../Shaders/point.vs", "../Shaders/point.fs");
     viewer.addProgram("surface", "../Shaders/surface.vs", "../Shaders/surface.fs");
+    
+    HermiteSurface herm_surf(InterpolationMode::hermite_from_ctrl_pts);
+    SurfacePtr surf_ptr = std::make_shared<HermiteSurface>(herm_surf);
+    viewer.addSurface(surf_ptr, 20, 20);
 
-    viewer.addSurface(std::make_shared<Grid>(
-        glm::vec3(-0.8f, 0.8f, 0.0f),
-        glm::vec3(0.5f, 0.5f, 0.0f),
-        glm::vec3(-0.5f, -0.5f, 0.0f),
-        glm::vec3(0.8f, -0.8f, 0.0f)
+   viewer.addSurface(std::make_shared<Grid>(
+        glm::vec3(-0.8f, 0.9f, 0.0f),
+        glm::vec3(0.5f, 0.6f, 0.0f),
+        glm::vec3(-0.5f, -0.4f, 0.0f),
+        glm::vec3(0.8f, -0.7f, 0.0f)
     ));
 
     while (viewer.isRunning())
